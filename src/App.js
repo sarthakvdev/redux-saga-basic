9e4bf73d199f691da05d76812a2559a3e581b433
Sarthak Verma <55709030+sarthakvdev@users.noreply.github.com>
2021-07-22 18:03:15 +0530

    basic redux voting app

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import Counter from "./components/counter";
 import "./styles.css";
 
 export default function App() {
-  // importing the state variable from store
-  // using useSelector hook
-  const count = useSelector((state) => state.counter.count);
+  // importing the state variable from store: useSelector hook
+  const people = [
+    "Bruce Wayne",
+    "Selina Kyle",
+    "Bob Dylan",
+    "Steve Jobs",
+    "Harvey Dent"
+  ];
 
+  const count = useSelector((state) => state.counter.count);
   return (
     <div className="App">
-      <h2>Count: {count}</h2>
-      <Counter />
+      <h2>Total Votes: {count}</h2>
+      {people.map((name) => (
+        <Counter name={name} />
+      ))}
     </div>
   );
 }
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,25 +1,33 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
+
 // actions
 import { increment, decrement } from "../redux/ducks/counter";
 
-const Counter = () => {
+const Counter = (props) => {
+  const [vote, setVote] = useState(0);
+
   // useDispatch hook for dispatching actions
   const dispatch = useDispatch();
-
+  const { name } = props;
   const handleIncrement = () => {
     dispatch(increment());
+    setVote(vote + 1);
   };
   const handleDecrement = () => {
     dispatch(decrement());
+    setVote(vote - 1);
   };
 
   return (
-    <div>
+    <div className="counterBox">
+      <h3>{name}</h3>
+      <h4>Votes: {vote}</h4>
       <button className="button" onClick={handleDecrement}>
-        decrement
+        -
       </button>
       <button className="button" onClick={handleIncrement}>
-        increment
+        +
       </button>
     </div>
   );
